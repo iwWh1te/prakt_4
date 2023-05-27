f71b24d2b3147392aa04177b0ba9ca97d6cf5eec
iwWh1te <ylanovru10@gmail.com>
2023-05-27 15:37:10 +0400

    4_incr

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 class TPen{
+	private:
+	std::string FColor;
 	public:
-	std::string color; 
+	void setColor(std::string newColor);
+	std::string getColor();
 };
+
+void TPen::setColor(std::string newColor){
+FColor= newColor;}
+
+std::string TPen::getColor(){
+return FColor;}
+	
 
 int main(){
 	using namespace std;
 	TPen pen;
-	pen.color = "FF00FF";
-	cout<<"pen color : "<< pen.color;
+	//pen.FColor = "FF00FF";
+	pen.setColor("FF00FF");
+	cout<<"pen color : "<< pen.getColor();
 	return 0;
 }
