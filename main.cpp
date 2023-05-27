93638dc4fe1fe8b207ba6639df98c3058ab76d6f
iwWh1te <ylanovru10@gmail.com>
2023-05-27 15:35:24 +0400

    3_incr

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-class TPen{};
+#include <iostream>
+class TPen{
+	public:
+	std::string color; 
+};
 
 int main(){
+	using namespace std;
 	TPen pen;
+	pen.color = "FF00FF";
+	cout<<"pen color : "<< pen.color;
+	return 0;
 }
